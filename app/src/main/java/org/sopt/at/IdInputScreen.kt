50d02609e6d76f0a42a5bb048760ec6b2a13a61a
Hyejeong <myejeong0211@gmail.com>
2025-04-09 17:45:17 +0900

    feat: 회원가입 아이디 유효성 검사

--- app/src/main/java/org/sopt/at/IdInputScreen.kt
+++ app/src/main/java/org/sopt/at/IdInputScreen.kt
@@ -10,14 +10,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @Composable
 fun IdInputScreen(
@@ -26,51 +32,74 @@ fun IdInputScreen(
     onNext: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.Black)
-            .padding(horizontal = 15.dp, vertical = 100.dp),
-        verticalArrangement = Arrangement.SpaceBetween
-    ) {
+    // id 유효성 검사: 영문 소문자 또는 영문 소문자, 숫자 조합 6~12자리
+    val isValidId = id.matches(Regex("^[a-z0-9]{6,12}$"))
+
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        }
+    ) { innerPadding ->
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Black)
+                .padding(innerPadding)
+                .padding(horizontal = 15.dp, vertical = 100.dp),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(
-                text = "아이디를 입력해주세요.",
-                color = Color.White,
-                fontSize = 20.sp
-            )
-
-            Column {
-                TextField(
-                    value = id,
-                    onValueChange = onIdChange,
-                    placeholder = { Text("아이디") },
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .padding(top = 20.dp)
-                )
-
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Text(
-                    text = "영문 소문자 또는 영문 소문자, 숫자 조합 6~12 자리",
-                    color = Color.White
+                    text = "아이디를 입력해주세요.",
+                    color = Color.White,
+                    fontSize = 20.sp
                 )
+
+                Column {
+                    // 아이디 입력 창
+                    TextField(
+                        value = id,
+                        onValueChange = onIdChange,
+                        placeholder = { Text("아이디") },
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(top = 20.dp)
+                    )
+
+                    Text(
+                        text = "영문 소문자 또는 영문 소문자, 숫자 조합 6~12 자리",
+                        color = Color.White
+                    )
+                }
+            }
+
+            Button(
+                onClick =  {
+                    if (isValidId) {
+                        onNext()
+                    } else {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("ID가 유효하지 않습니다.")
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, Color.Gray),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.LightGray
+                )
+            ) {
+                Text("다음")
             }
         }
 
-        Button(
-            onClick = onNext,
-            modifier = Modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(10.dp),
-            border = BorderStroke(1.dp, Color.Gray),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Black,
-                contentColor = Color.LightGray
-            )
-        ) {
-            Text("다음")
-        }
     }
 
 }
