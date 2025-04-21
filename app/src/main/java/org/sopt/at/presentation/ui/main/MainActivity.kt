238eaa2a93a4dceb931938dd8551869ced1645ba
Hyejeong <myejeong0211@gmail.com>
2025-04-21 22:54:38 +0900

    feat: 상태바 밑으로 화면

--- app/src/main/java/org/sopt/at/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/main/MainActivity.kt
@@ -5,11 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
+import androidx.core.view.WindowCompat
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
         setContent {
             ATSOPTANDROIDTheme {
                 MainScreen()
