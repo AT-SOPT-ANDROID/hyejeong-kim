5326082d1bcabdabab3357ef4007f274f39cc416
Hyejeong <myejeong0211@gmail.com>
2025-04-25 00:33:18 +0900

    design: dialog 디자인 수정

--- app/src/main/java/org/sopt/at/core/component/dialog/HistoryDialog.kt
+++ app/src/main/java/org/sopt/at/core/component/dialog/HistoryDialog.kt
@@ -1,15 +1,28 @@
 package org.sopt.at.core.component.dialog
 
-import androidx.compose.foundation.background
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import org.sopt.at.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -20,33 +33,62 @@ fun HistoryDialog(
     onDeleteClick: () -> Unit = {},
     onDismissRequest: () -> Unit = {}
 ) {
-    AlertDialog(
+    Dialog(
         onDismissRequest = onDismissRequest,
-        confirmButton = {
-            TextButton(
-                onClick = {
-                    onAddClick()
-                }
-            ) {
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            shape = RoundedCornerShape(12.dp),
+            color = Color.White
+        ) {
+            Column {
+                Spacer(
+                    modifier = Modifier
+                        .height(12.dp)
+                        .fillMaxWidth()
+                )
                 Text(
-                    text = stringResource(R.string.history_dialog_add)
+                    text = stringResource(R.string.history_dialog_add_series),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentSize()
+                        .padding(vertical = 8.dp),
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(
+                    modifier = Modifier
+                        .height(12.dp)
+                        .fillMaxWidth()
+                )
+                Button(
+                    onClick = {
+                        onAddClick()
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    shape = RoundedCornerShape(24.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black
+                    )
+                ) {
+                    Text(
+                        text = stringResource(R.string.history_dialog_add),
+                        fontSize = 16.sp
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .height(12.dp)
+                        .fillMaxWidth()
                 )
             }
-        },
-        modifier = Modifier.background(
-            color = Color.White
-        ),
-        title = {
-            Text(
-                text = stringResource(R.string.history_dialog_add_series)
-            )
-        },
-        text = {
-            Text(
-                text = stringResource(R.string.history_dialog_add_series_question)
-            )
         }
-    )
+    }
 }
 
 @Preview
