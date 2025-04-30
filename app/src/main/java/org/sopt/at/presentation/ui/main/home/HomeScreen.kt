62af2787877d6d5f5af6f24dab9dd2f9a6fa0293
Hyejeong <myejeong0211@gmail.com>
2025-04-30 16:03:07 +0900

    feat: 홈 상단 탭 클릭 시 맨 위 아이템으로 이동

--- app/src/main/java/org/sopt/at/presentation/ui/main/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/main/home/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Tab
@@ -16,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.launch
 import org.sopt.at.R
 import org.sopt.at.core.component.list.HomeList
 import org.sopt.at.core.component.list.HomeListCardItem
@@ -47,10 +50,16 @@ fun HomeScreen(
         initialPage = 0
     )
 
+    // list 상태
+    val listState = rememberLazyListState()
+
+    val coroutineScope = rememberCoroutineScope()
+
     LazyColumn(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxSize()
-            .background(color = Color.Black)
+            .background(color = Color.Black),
+        state = listState
     ) {
         stickyHeader {
             TabRow(
@@ -64,7 +73,12 @@ fun HomeScreen(
                 tabSections.forEachIndexed { index, resId ->
                     Tab(
                         selected = selectedTabIndex == index,
-                        onClick = { viewModel.selectTab(index) },
+                        onClick = {
+                            viewModel.selectTab(index)
+                            coroutineScope.launch {
+                                listState.animateScrollToItem(0)
+                            }
+                        },
                     ) {
                         Box(
                             modifier = Modifier
