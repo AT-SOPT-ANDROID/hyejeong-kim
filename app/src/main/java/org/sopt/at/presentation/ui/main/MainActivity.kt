8f046c57db1210fae23356b72ca868ad46939342
Hyejeong <myejeong0211@gmail.com>
2025-05-02 00:01:22 +0900

    feat: MainActivity Theme 적용

--- app/src/main/java/org/sopt/at/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/org/sopt/at/presentation/ui/main/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.at.core.navigation.LocalNavController
-import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
+import org.sopt.at.ui.theme.TvingTheme
 
 // Application 클래스에 Hilt를 설정하고 애플리케이션 수준 구성요소를 사용할 수 있게 되면 Hilt는 이 주석이 있는 다른 Android 클래스에 종송 항목 제공 가능
 @AndroidEntryPoint
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             CompositionLocalProvider(LocalNavController provides navController) {
-                ATSOPTANDROIDTheme {
+                TvingTheme {
                     MainScreen()
                 }
             }
